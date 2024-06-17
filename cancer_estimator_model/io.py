8ee745fe13cf5006b9641f8695f5eba1083de9da
Manoel V. Machado <manoelnt0@gmail.com>
2024-06-16 22:43:58 -0300

    Add custom build of catboost compatible with LGA775 (#46)
    * Refacto datasets module to be compatible with jupyter yet
    
    * Mark some artificial variables as not categorical
    
    * Add custom catboost build and move sklearn to dev dep
    
    * Update pdm.lock

--- cancer_estimator_model/io.py
+++ cancer_estimator_model/io.py
@@ -4,3 +4,17 @@ import pandas
 def dict_to_pandas(features: dict) -> pandas.DataFrame:
     features_uppercase = {k.upper(): v for k, v in features.items()}
     return pandas.json_normalize(features_uppercase)
+
+
+def is_notebook() -> bool:
+    # ref: https://stackoverflow.com/a/39662359/3749971
+    try:
+        shell = get_ipython().__class__.__name__  # type: ignore
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
